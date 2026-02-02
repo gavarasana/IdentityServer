0b0392c24d53da18e6ee9bcd182f6ce916da9398
Ravi Gavarasana <gavarasana@gmail.com>
2026-02-01 20:40:42 -0500

    Replaced SQL Lite with SQL Server

---
+++ Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,7 @@
+namespace Hobron.IdentityServer.Pages.Logout;
+
+public static class LogoutOptions
+{
+    public static readonly bool ShowLogoutPrompt = true;
+    public static readonly bool AutomaticRedirectAfterSignOut = false;
+}
